3f47f5e87f52d07ad7530a0edef324a83b629836
nao <nurettinakinoral@gmail.com>
2022-06-15 19:36:36 +0300

    going on

--- src/App.js
+++ src/App.js
@@ -4,22 +4,60 @@ import Alert from './Alert';
 
 function App() {
   const [name, setName] = useState('');
-  const [list, seList] = useState([]);
+  const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  });
   const handleSubmit = e => {
     e.preventDefault();
-    console.log('hello');
+    if (!name) {
+      showAlert(true, 'danger', 'Please Enter a Value');
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      showAlert(true, 'success', 'item added to the list');
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
+  const showAlert = (show = false, type = '', msg = '') => {
+    setAlert({ show, type, msg });
   };
 
   return (
     <section className="section-center">
-      <form className="grocery-form" onSubmit={handleSubmit}></form>
-      <div className="grocety-container">
-        <List />
-        <button className="clear-btn">Clear Items</button>
-      </div>
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
